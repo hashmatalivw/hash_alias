990c98edd4f142cc904ecdce9768240c70c2f249
hashmatalivw <hashmatali@viralwebbs.com>
2020-12-15 11:25:49 +0500

    first commit

---
+++ hash.cmd
@@ -0,0 +1,53 @@
+@ECHO OFF
+
+doskey ..=cd ..
+doskey h=C: ^& cd %USERPROFILE%
+
+doskey pa=php artisan $*
+doskey pu=.\vendor\bin\phpunit $*
+doskey pas=php artisan serve
+doskey pam=php artisan migrate
+doskey pamr=php artisan migrate:refresh
+doskey seed=php artisan db:seed
+
+doskey c=composer $*
+doskey cda=composer dump-autoload $*
+doskey cda=composer update
+doskey cda=composer install
+echo.
+echo.
+echo ----       ----                ------            /-----------\     ----       ----   
+echo \   \       \   \             /  /\  \          /  /-------\  \    \   \       \   \
+echo  \   \------ \   \           /  /  \  \         \  \                \   \------ \   \  
+echo   \   \------ \   \         /  /----\  \         \  \----------\     \   \------ \   \
+echo    \   \       \   \       /  /------\  \         ----------/  /      \   \       \   \
+echo     \   \       \   \     /  /        \  \     ----------- /  /        \   \       \   \
+echo       ----        ----    ---          ---     \  \-------/  /           ----        ----
+echo.
+echo.
+
+echo Hashmat Console v0.0.1
+echo Work Hard In Silence, Let Your Success Be Your Noise
+echo.
+echo -----------------------------------------------
+echo \\            List of basic commands         \\
+echo -----------------------------------------------
+echo    ..          back
+echo    h           user home
+echo.
+echo -----------------------------------------------
+echo \\           List of artisan commands        \\
+echo -----------------------------------------------
+echo    pa          php artisan
+echo    pas         php artisan serve
+echo    pam         php artisan migrate
+echo    pamr        php artisan migrate:refresh
+echo    seed        php artisan db:seed
+echo.                                           
+echo -----------------------------------------------
+echo \\          List of composer commands        s\\
+echo -----------------------------------------------
+echo    c           composer
+echo    cda         composer dump-autoload
+echo    cu          composer update
+echo    ci          composer install
